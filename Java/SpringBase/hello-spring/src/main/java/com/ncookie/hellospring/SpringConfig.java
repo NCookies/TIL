d4d24e2890d608ced215a8b33a7f6af693e285b2
NCookies <swstar21c@gmail.com>
2022-10-14 03:41:43 +0900

    [스프링 입문] 섹션6 JPA까지 완료

--- Java/SpringBase/hello-spring/src/main/java/com/ncookie/hellospring/SpringConfig.java
+++ Java/SpringBase/hello-spring/src/main/java/com/ncookie/hellospring/SpringConfig.java
@@ -1,13 +1,29 @@
 package com.ncookie.hellospring;
 
-import com.ncookie.hellospring.repository.MemberRepository;
-import com.ncookie.hellospring.repository.MemoryMemberRepository;
+import com.ncookie.hellospring.repository.*;
 import com.ncookie.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+//    private final DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +32,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
