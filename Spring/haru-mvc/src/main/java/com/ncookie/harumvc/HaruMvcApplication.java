94d2b37862c11c80e7e2cc0d9d18b69aaedf2c2f
NCookies <swstar21c@gmail.com>
2022-10-14 22:55:54 +0900

    [HARU MVC] init commit

---
+++ Spring/haru-mvc/src/main/java/com/ncookie/harumvc/HaruMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ncookie.harumvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruMvcApplication.class, args);
+	}
+
+}
