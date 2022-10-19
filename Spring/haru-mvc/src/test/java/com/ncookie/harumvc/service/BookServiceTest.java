5fecf3bf6e4ca95d28fae0d8dd7b698fd6f9c7b6
NCookies <swstar21c@gmail.com>
2022-10-19 23:01:51 +0900

    [HARU MVC] 테스트 클래스 생성

---
+++ Spring/haru-mvc/src/test/java/com/ncookie/harumvc/service/BookServiceTest.java
@@ -0,0 +1,28 @@
+package com.ncookie.harumvc.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByKeyword() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
