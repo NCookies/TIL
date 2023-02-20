7658720bbf3533afa95b9f26dfdd6fe5cd8c6eff
NCookies <swstar21c@gmail.com>
2023-02-20 13:23:58 +0900

    [baekjoon] 11653번 소인수분해 문제 - 2
    - 기존의 방법은 너무 비효율적이었음
    - for문과 while문의 위치를 바꿔주니 훨씬 빠르게 동작함

--- Algorithm/Baekjoon/src/main/java/Level/Level08_기본수학2/P11653/Main.java
+++ Algorithm/Baekjoon/src/main/java/Level/Level08_기본수학2/P11653/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static boolean[] prime;      // 에라토스테네스 체 배열
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -15,34 +14,16 @@ public class Main {
             return;
         }
 
-        prime = new boolean[n + 1];
-        makeFilter();
-
-
-        while (prime[n]) {      // n이 소수가 될 때까지 루프
-            for (int i = 2; i < n; i++) {
-                if (!prime[i] && n % i == 0) {
-                    sb.append(i).append('\n');
-                    n = n / i;
-                    break;
-                }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                sb.append(i).append("\n");
+                n = n / i;
             }
         }
-        sb.append(n);
 
+        if (n != 1) {
+            sb.append(n);
+        }
         System.out.println(sb);
-    }
-
-    // 에라토스테네스 체 알고리즘
-    // false == 소수
-    public static void makeFilter() {
-        prime[0] = true;
-        prime[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
-            for (int j = i * i; j < prime.length; j+= i) {
-                prime[j] = true;
-            }
-        }
     }
 }
