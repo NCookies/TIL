ccccba0411d134075c533d017b6c8660162427b3
NCookies <swstar21c@gmail.com>
2023-02-24 22:52:23 +0900

    [baekjoon] 25314번 코딩은 체육과목 입니다 문제 풀이 완료

---
+++ Algorithm/Baekjoon/src/main/java/Level/Level03_반복문/P25314/Main.java
@@ -0,0 +1,21 @@
+package Level.Level03_반복문.P25314;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n / 4; i++) {
+            sb.append("long").append(' ');
+        }
+        sb.append("int");
+
+        System.out.println(sb);
+    }
+}
