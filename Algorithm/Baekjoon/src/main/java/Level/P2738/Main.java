8c2f8b76746196ce0e9addaddd31a53098152920
NCookies <swstar21c@gmail.com>
2023-03-05 21:10:01 +0900

    [baekjoon] 2738번 행렬 덧셈 문제 풀이완료 - 1
    - 시간 너무 오래 걸림

---
+++ Algorithm/Baekjoon/src/main/java/Level/P2738/Main.java
@@ -0,0 +1,36 @@
+package Level.P2738;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 행렬 덧셈
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken())).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
