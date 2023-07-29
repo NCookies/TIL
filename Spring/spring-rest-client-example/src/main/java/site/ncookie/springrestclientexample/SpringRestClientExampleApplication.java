cb1a5239cb1bb62ccf1a861249e179f8746b1258
NCookies <swstar21c@gmail.com>
2023-07-29 18:25:22 +0900

    #3 - Hello World!

--- Spring/spring-rest-client-example/src/main/java/site/ncookie/springrestclientexample/SpringRestClientExampleApplication.java
+++ Spring/spring-rest-client-example/src/main/java/site/ncookie/springrestclientexample/SpringRestClientExampleApplication.java
@@ -1,10 +1,18 @@
 package site.ncookie.springrestclientexample;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestClientExampleApplication {
+    @Bean
+    ApplicationRunner init() {
+        return  args -> {
+            System.out.println("Hello World");
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestClientExampleApplication.class, args);
