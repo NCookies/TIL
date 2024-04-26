091c1e9cf8f6cf162e47d1c01a9bb31f8fc9646f
NCookies <swstar21c@gmail.com>
2024-04-26 23:57:24 +0900

    [알고리즘/백준] 28116 문제 코드 추가 및 시간 초과

---
+++ CS/Algorithm/Basic/baekjoon/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] answer = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int min = arr[i];
+            int minIndex = i;
+            
+            for (int j = i; j < N; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+
+            if (i == minIndex && i < (N - 1)) {
+                answer[arr[i] - 1] = 0;
+            } else {
+                answer[arr[i] - 1] += (minIndex - i);
+                answer[arr[minIndex] - 1] += (minIndex - i);
+
+                // swap
+                int tmp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = tmp;
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            sb.append(answer[k]).append(' ');
+        }
+
+        System.out.println(sb);
+    }
+}
