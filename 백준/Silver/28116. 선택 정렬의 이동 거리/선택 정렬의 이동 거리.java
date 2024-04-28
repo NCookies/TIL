e99804f9e8db7052bd064e1d717a75da434ca2be
SeungWoo Ryu <swstar21c@gmail.com>
2024-04-28 11:24:03 +0900

    [Silver III] Title: 선택 정렬의 이동 거리, Time: 664 ms, Memory: 72956 KB -BaekjoonHub

--- 백준/Silver/28116. 선택 정렬의 이동 거리/선택 정렬의 이동 거리.java
+++ 백준/Silver/28116. 선택 정렬의 이동 거리/선택 정렬의 이동 거리.java
@@ -1,10 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
-import java.util.Map;
 
 
 public class Main {
@@ -14,11 +11,9 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        // int[] arr = new int[N];
 
-        // key(수열의 값)과 value(값이 저장된 인덱스)로 이루어진 맵 자료구조
-        // 수열의 값이 몇 번째 인덱스에 있는지 알 수 있음
-        Map<Integer,Integer> numMap = new HashMap<Integer,Integer>();
+        // 각 숫자가 몇 번째 인덱스에 있는지 저장하고 있는 배열
+        int[] numArr = new int[N + 1];
 
         // 수열 A를 저장하고 있는 배열
         int[] idxArr = new int[N + 1];
@@ -27,10 +22,10 @@ public class Main {
         int[] answer = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            numMap.put(num, i + 1);
-            idxArr[i + 1] = num;
+            numArr[num] = i;
+            idxArr[i] = num;
         }
 
         // 수열 A는 1부터 N까지 있으므로 1부터 시작
@@ -38,7 +33,7 @@ public class Main {
             // 현재 최소값이 와야하는 위치의 값이 최소값이 아닌 경우
             if (idxArr[i] != i) {
                 // 정렬되지 않은 부분의 최소값이 저장된 위치
-                int minIndex = numMap.get(i);
+                int minIndex = numArr[i];
 
                 // 최소값이 저장된 위치와 현재 정렬하려는 위치 간의 거리
                 int distance = minIndex - i;
@@ -48,10 +43,11 @@ public class Main {
                 answer[idxArr[minIndex]] += distance;
 
                 // 맵의 key-value 스왑
-                int tmp = numMap.get(i);
-                numMap.put(i, numMap.get(idxArr[i]));
-                numMap.put(idxArr[i], tmp);
+                int tmp = numArr[i];
+                numArr[i] = numArr[idxArr[i]];
+                numArr[idxArr[i]] = tmp;
 
+                // idx 배열 스왑
                 tmp = idxArr[i];
                 idxArr[i] = idxArr[minIndex];
                 idxArr[minIndex] = tmp;
