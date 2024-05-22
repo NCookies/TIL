e2d353d89b9983063422ee700bf2ac90eb64660b
SeungWoo Ryu <swstar21c@gmail.com>
2024-05-22 22:56:44 +0900

    [Bronze I] Title: 단어 공부, Time: 156 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int str = System.in.read();
+        int[] arr = new int[26];
+
+        while (str > 64) {
+            if (str >= 97) {
+                str -= 32;
+            }
+
+            arr[str - 65]++;
+            str = System.in.read();
+        }
+
+        int max = -1;
+        int max_idx = -1;
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                max_idx = i;
+            }
+        }
+        for (int i : arr) {
+            if (i == max) {
+                cnt++;
+            }
+        }
+
+        if (cnt >= 2) {
+            System.out.println("?");
+        } else {
+            System.out.println((char) (max_idx + 65));
+        }
+    }
+}
